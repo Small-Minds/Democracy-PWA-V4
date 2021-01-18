12ce7c1d2ee5c92e2773147abc0ddbe1fd8552d2
Ryan Fleck <ryan.fleck@protonmail.com>
2021-01-18 14:58:05 -0500

    Change import notation.

--- src/utils/test/authentication.test.tsx
+++ src/utils/test/authentication.test.tsx
@@ -1,4 +1,6 @@
-import { add } from '../Authentication';
+import { add } from './../Authentication';
+
+// Push... Github CI Can't find /Auth... file?
 
 test('renders vote link', () => {
   expect(add(1, 2)).toBe(3);

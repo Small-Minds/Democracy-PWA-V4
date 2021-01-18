9220d3c73cb3adb7c610036dd2db220f61e85297
Ryan Fleck <ryan.fleck@protonmail.com>
2021-01-18 14:13:39 -0500

    Set up utility files.

---
+++ src/utils/test/authentication.test.tsx
@@ -0,0 +1,5 @@
+import { add } from '../authentication';
+
+test('renders vote link', () => {
+  expect(add(1, 2)).toBe(3);
+});
---
+++ src/utils/test/index.test.tsx
@@ -0,0 +1,5 @@
+import { add } from '../index';
+
+test('renders vote link', () => {
+  expect(add(1, 2)).toBe(3);
+});

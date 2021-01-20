86571f390aa06be687343d8853f0bfd1d28ab848
Ryan Fleck <ryan.fleck@protonmail.com>
2021-01-20 12:04:55 -0500

    Continue form implementation.

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Credentials } from '../utils/Authentication';
 import {
   Form,
@@ -9,20 +9,50 @@ import {
   ButtonToolbar,
   Button,
   Panel,
+  Schema,
 } from 'rsuite';
 
-/**
- * Here is an example of a useContext hook to consume a provider.
- */
+const msg_required = 'This field is required.';
+const model = Schema.Model({
+  name: Schema.Types.StringType().isRequired(msg_required),
+  username: Schema.Types.StringType().isRequired(msg_required),
+  email_address: Schema.Types.StringType()
+    .isEmail('Please use a valid email address.')
+    .isRequired(msg_required),
+  password1: Schema.Types.StringType().isRequired(msg_required),
+  password2: Schema.Types.StringType().isRequired(msg_required),
+});
+
 function SignupForm() {
   // When the app first starts, it is unauthenticated.
   const ctx = useContext(Credentials);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [disabled, setDisabled] = useState<boolean>(false);
+
+  const [formData, setFormData] = useState<any>({
+    name: '',
+    username: '',
+    email_address: '',
+    password1: '',
+    password2: '',
+  });
+  const [formErrors, setFormErrors] = useState<any>({});
+
+  const submitFormData = async () => {
+    console.log(formData);
+  };
 
   return (
     <div>
       <Panel header={<h2>Sign Up</h2>} bordered>
-        <Form>
-          <FormGroup>
+        <Form
+          onChange={(newData) => setFormData(newData)}
+          onCheck={(newErrors) => setFormErrors(newErrors)}
+          formValue={formData}
+          formError={formErrors}
+          model={model}
+        >
+          <FormGroup controlId="name">
             <ControlLabel>First &amp; Last Name</ControlLabel>
             <FormControl name="name" />
           </FormGroup>
@@ -44,13 +74,18 @@ function SignupForm() {
             <FormControl name="password1" type="password" />
           </FormGroup>
           <FormGroup>
-            <ControlLabel>Verify Password</ControlLabel>
             <FormControl name="password2" type="password" />
+            <HelpBlock>Enter your password twice.</HelpBlock>
           </FormGroup>
           <FormGroup>
             <ButtonToolbar>
-              <Button appearance="primary">Submit</Button>
-              <Button appearance="default">Cancel</Button>
+              <Button
+                appearance="primary"
+                loading={loading}
+                onClick={submitFormData}
+              >
+                Submit
+              </Button>
             </ButtonToolbar>
           </FormGroup>
         </Form>

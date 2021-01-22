867fb6e46bdace938287eba9a5f3cdf763e80a70
MengxuanChen <mchen210@uottawa.ca>
2021-01-22 11:26:56 -0500

    Added Chinese and French Translation. Implementd translation mechanism

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 import { Panel, SelectPicker } from 'rsuite';
 export default function LanguagePicker() {
-    const languageCollection = [{"label": "English", "value":"en"},{"label": "French", "value":"fr"},{"label": "Chinese", "value":"cn"}]
+    //Set Up translation hook
+    const [t,i18n] = useTranslation();
+    const languageCollection = [{"label": t("languagePicker.en"), "value":"en"},
+                                {"label": t("languagePicker.fr"), "value":"fr"},
+                                {"label": t("languagePicker.cn"), "value":"cn"}];
+
+    function toggleLanguage (language:string){
+        i18n.changeLanguage(language);
+    }
+
     return (
         <div>
-            <Panel header={<h2>Language Selection</h2>} bordered>
-                <SelectPicker data={languageCollection} onChange={(value:string) => console.log(value)}/>
+            <Panel header={<h3>{t("languagePicker.title")}</h3>} bordered>
+                <SelectPicker data={languageCollection} onChange={(language:string) => toggleLanguage(language)}/>
             </Panel>    
         </div>
     )

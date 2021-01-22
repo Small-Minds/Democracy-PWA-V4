f1642cdfef99f19653da39c01f5cc82ed86cd4db
Ryan Fleck <ryan.fleck@protonmail.com>
2021-01-22 13:52:56 -0500

    Update LanguagePicker

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Panel, SelectPicker } from 'rsuite';
+import { Panel, Dropdown, ButtonToolbar, Navbar } from 'rsuite';
 export default function LanguagePicker() {
   //Set Up translation hook
   const [t, i18n] = useTranslation();
+  const [language, setLanguage] = useState<string | undefined>();
   const languageCollection = [
     { label: t('languagePicker.en'), value: 'en' },
     { label: t('languagePicker.fr'), value: 'fr' },
     { label: t('languagePicker.cn'), value: 'cn' },
   ];
 
-  function toggleLanguage(language: string) {
-    i18n.changeLanguage(language);
-  }
+  useEffect(() => {
+    i18n.changeLanguage(language || 'en');
+  }, [language, setLanguage]);
 
   return (
     <div>
-      <Panel header={<h3>{t('languagePicker.title')}</h3>} bordered>
-        <SelectPicker
-          data={languageCollection}
-          onChange={(language: string) => toggleLanguage(language)}
-        />
-      </Panel>
+      <div>
+        <Dropdown
+          title={t(`languagePicker.${language || 'en'}`)}
+          activeKey={language || 'en'}
+          onSelect={(key) => setLanguage(key)}
+        >
+          {languageCollection.map((lang, key) => (
+            <Dropdown.Item key={key} eventKey={lang.value}>
+              {lang.label}
+            </Dropdown.Item>
+          ))}
+        </Dropdown>
+      </div>
+      <br />
     </div>
   );
 }

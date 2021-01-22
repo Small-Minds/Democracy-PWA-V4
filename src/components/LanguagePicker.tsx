b1c131ec612d20c2cd2bdda14b19df369bd60a7c
Ryan Fleck <ryan.fleck@protonmail.com>
2021-01-22 14:34:19 -0500

    Attempt to fix language detection.

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -4,24 +4,11 @@ import { Panel, Dropdown, ButtonToolbar, Navbar, Icon } from 'rsuite';
 export default function LanguagePicker() {
   //Set Up translation hook
   const [t, i18n] = useTranslation();
-  const [language, setLanguage] = useState<string | undefined>();
   const languageCollection = [
     { label: t('languagePicker.en'), value: 'en' },
     { label: t('languagePicker.fr'), value: 'fr' },
     { label: t('languagePicker.cn'), value: 'cn' },
   ];
-
-  // TODO: This logic should probably be moved up, into a provider, to prevent
-  // a flash of English text when the page is first reloaded.
-  useEffect(() => {
-    if (!language || language === '') {
-      const storedLang = localStorage.getItem('language');
-      if (storedLang) setLanguage(storedLang);
-    } else {
-      i18n.changeLanguage(language || 'en');
-      localStorage.setItem('language', language);
-    }
-  }, [language, setLanguage]);
 
   return (
     <div>
@@ -29,9 +16,9 @@ export default function LanguagePicker() {
         <Dropdown
           size="lg"
           icon={<Icon icon="globe2" />}
-          title={t(`languagePicker.${language || 'en'}`)}
-          activeKey={language || 'en'}
-          onSelect={(key) => setLanguage(key)}
+          title={t(`languagePicker.${i18n.language || 'en'}`)}
+          activeKey={i18n.language || 'en'}
+          onSelect={(key) => i18n.changeLanguage(key)}
         >
           {languageCollection.map((lang, key) => (
             <Dropdown.Item key={key} eventKey={lang.value}>

80dc2e44da8228bb4ed4f8b262f98809afc657fc
Ryan Fleck <ryan.fleck@protonmail.com>
2021-01-22 14:40:36 -0500

    Fix i18n config.

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,14 +1,12 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import XHR from 'i18next-http-backend';
 
 import languageEN from './locale/en/translation.json';
 import languageCN from './locale/cn/translation.json';
 import languageFR from './locale/fr/translation.json';
 
 i18n
-  .use(XHR)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({

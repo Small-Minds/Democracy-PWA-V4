72c27e1fde723376845f7166113f1f8e1de14e58
Max <mchen210@uottawa.ca>
2021-02-03 13:08:11 -0500

    add authentication check before rendering Election list

--- src/components/ElectionList.tsx
+++ src/components/ElectionList.tsx
@@ -1,27 +1,39 @@
 import React, { useEffect, useContext, useState} from 'react'
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { List } from 'rsuite'
 import { getElectionList, Election} from '../utils/api/ElectionManagement'
 import { getAccessToken} from '../utils/API';
-import ElectionInfo from './ElectionInfo';
+import { Credentials } from '../utils/Authentication';
+
 
 export default function ElectionList() {
-    const accessToken = getAccessToken();
+
+    const ctx = useContext(Credentials);
     const[isLoading, setLoading] = useState(true);
+    const[notLoggedin, setLoggedin] = useState(true);
     const[electionList, setElectionList] = useState<Array<Election>>([]);
     
     useEffect(()=>{
-        accessToken ? 
-        getElectionList(accessToken.token)
-        .then((res)=>{
-            setElectionList(res.data);
-            setLoading(false);
-        }) : console.log('Please Login');
-    }, [])
+        ctx ? 
+        //Check whether user is authenticated or not
+        (ctx.credentials.authenticated ?
+        // If user is authenticated, the compoenet will render the election list
+        ( 
+          getElectionList(ctx.credentials.token)
+         .then((res)=>{
+                setElectionList(res.data);
+                setLoading(false);
+        })): setLoggedin(false))
+        : console.log('Please Login');
+    }, [isLoading])
 
     //waiting for the response from getEletctionList 
     if(isLoading){
-        return <div>Loading...</div>
+        if(notLoggedin){
+            return <div>Please Login</div>
+        }else{
+            return <div>Loading...</div>
+        }
     }
 
     return (

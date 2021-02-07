f7ab53020745ee750651cf3666a788bc268ffc5e
Ryan Fleck <ryan.fleck@protonmail.com>
2021-02-06 19:06:22 -0500

    Simplify ElectionList

--- src/components/ElectionList.tsx
+++ src/components/ElectionList.tsx
@@ -1,50 +1,33 @@
-import React, { useEffect, useContext, useState} from 'react'
+import React, { useEffect, useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { List } from 'rsuite'
-import { getElectionList, Election} from '../utils/api/ElectionManagement'
-import { getAccessToken} from '../utils/API';
+import { List } from 'rsuite';
+import { getElectionList, Election } from '../utils/api/ElectionManagement';
 import { Credentials } from '../utils/Authentication';
 
-
 export default function ElectionList() {
+  const ctx = useContext(Credentials);
+  const [electionList, setElectionList] = useState<Array<Election>>([]);
 
-    const ctx = useContext(Credentials);
-    const[isLoading, setLoading] = useState(true);
-    const[notLoggedin, setLoggedin] = useState(true);
-    const[electionList, setElectionList] = useState<Array<Election>>([]);
-    
-    useEffect(()=>{
-        ctx ? 
-        //Check whether user is authenticated or not
-        (ctx.credentials.authenticated ?
-        // If user is authenticated, the compoenet will render the election list
-        ( 
-          getElectionList(ctx.credentials.token)
-         .then((res)=>{
-                setElectionList(res.data);
-                setLoading(false);
-        })): setLoggedin(false))
-        : console.log('Please Login');
-    }, [isLoading])
+  useEffect(() => {
+    // Return early if no context is provided.
+    if (!ctx || !ctx.credentials.authenticated) return;
+    // Return if the list has already been populated.
+    if (electionList.length > 0) return;
+    // If logged in, attempt to get the list of elections.
+    getElectionList(ctx.credentials.token).then((res) => {
+      setElectionList(res.data);
+    });
+  }, [ctx]);
 
-    //waiting for the response from getEletctionList 
-    if(isLoading){
-        if(notLoggedin){
-            return <div>Please Login</div>
-        }else{
-            return <div>Loading...</div>
-        }
-    }
-
-    return (
-        <div>
-            <List>
-            {electionList.map((election, index) => (
-                    <List.Item key={index} index={index}>
-                        <Link to={`/election/${election.id}`}>{election.id}</Link>
-                    </List.Item>
-                ))}
-            </List>
-        </div>
-    )
+  return (
+    <div>
+      <List>
+        {electionList.map((election, index) => (
+          <List.Item key={index} index={index}>
+            <Link to={`/election/${election.id}`}>{election.id}</Link>
+          </List.Item>
+        ))}
+      </List>
+    </div>
+  );
 }

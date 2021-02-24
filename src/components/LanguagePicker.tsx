1121cf9464f8f029688ead15021a301c425179f8
Ryan Fleck <ryan.fleck@protonmail.com>
2021-02-24 09:32:14 -0500

    Remove unneccessary components from language picker.

--- src/components/LanguagePicker.tsx
+++ src/components/LanguagePicker.tsx
@@ -1,22 +1,12 @@
-import React, { Fragment, useEffect, useMemo, useState } from 'react';
+import React, { Fragment } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Panel, Dropdown, ButtonToolbar, Navbar, Icon } from 'rsuite';
+import { Dropdown, Icon } from 'rsuite';
 import { locales } from '../i18n';
 
 export default function LanguagePicker() {
   //Set Up translation hook
   const [t, i18n] = useTranslation();
 
-  // Returns true in development.
-  // const dev: boolean = useMemo(() => {
-  //   return (
-  //     window.location.hostname === 'localhost' ||
-  //     window.location.hostname === '127.0.0.1' ||
-  //     window.location.hostname === ''
-  //   );
-  // }, []);
-
-  // TODO: Re-enable all other languages once
   return (
     <Fragment>
       <Dropdown

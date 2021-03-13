e3b7b18a49c4ac9383cd23e379c810bfc090ea11
Ryan Fleck <ryan.fleck@protonmail.com>
2021-03-13 18:36:38 -0500

    Improve ManageAccount page.

--- src/pages/ManageAccount.tsx
+++ src/pages/ManageAccount.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, useContext, useMemo } from 'react';
 import { Fade } from 'react-awesome-reveal';
 import Gravatar from 'react-gravatar';
 import { useTranslation } from 'react-i18next';
-import { Avatar } from 'rsuite';
+import { Avatar, FlexboxGrid, Icon } from 'rsuite';
 import ChangeNameForm from '../components/ChangeNameForm';
 import ChangePasswordForm from '../components/ChangePasswordForm';
 import { User } from '../utils/api/User';
@@ -16,42 +16,56 @@ export default function ManageAccount() {
     return <Gravatar email={user.user.email} size={60} rating="pg" />;
   }, [user]);
 
+  if (!user || !user.user) return null;
   return (
-    <Fragment>
-      <h1>{t('manageAccountPage.title')}</h1>
-      <p>{t('manageAccountPage.instruction')}</p>
-      <br />
-      <div>
-        <Avatar size="lg" style={{ marginTop: 20, marginBottom: 20 }}>
-          {userImage && (
-            <Fade triggerOnce duration={600} delay={100}>
-              <div>{userImage}</div>
-            </Fade>
-          )}
-        </Avatar>
-      </div>
-      <a
-        href="https://en.gravatar.com/"
-        rel="external nofollow"
-        target="_blank"
-      >
-        {t('manageAccountPage.changeProfileBtn')}
-      </a>
-      <br />
-      <br />
-      <br />
-      <h3>{t('signUpForm.nameInputLabel')}</h3>
-      <br />
-      <ChangeNameForm />
-      <br />
-      <br />
-      <h3>{t('resetPassword.form.password1')}</h3>
-      <br />
-      <ChangePasswordForm />
-      <br />
-      <br />
-      <br />
-      <br />
-    </Fragment>
+    <div>
+      <Fade cascade duration={200} damping={0.2}>
+        <div style={{ marginBottom: 20 }}>
+          <h1>{t('manageAccountPage.title')}</h1>
+          <p>{t('manageAccountPage.instruction')}</p>
+        </div>
+        <div style={{ marginBottom: 5 }}>
+          <FlexboxGrid justify="start" align="middle">
+            <FlexboxGrid.Item style={{ marginRight: 10 }}>
+              <Avatar size="lg" style={{ marginTop: 20, marginBottom: 20 }}>
+                {userImage && (
+                  <Fade triggerOnce duration={600} delay={100}>
+                    <div>{userImage}</div>
+                  </Fade>
+                )}
+              </Avatar>
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item>
+              <h3>{user.user.name}</h3>
+              <p>
+                <code>{user.user.email}</code>
+              </p>
+            </FlexboxGrid.Item>
+          </FlexboxGrid>
+        </div>
+        <div style={{ marginBottom: 40 }}>
+          <p>
+            <a
+              href="https://en.gravatar.com/"
+              rel="external nofollow"
+              target="_blank"
+            >
+              {t('manageAccountPage.changeProfileBtn')}
+              <Icon icon="external-link" style={{ marginLeft: 5 }}></Icon>
+            </a>
+          </p>
+        </div>
+        <div style={{ marginBottom: 30 }}>
+          <h3>{t('signUpForm.nameInputLabel')}</h3>
+          <br />
+          <ChangeNameForm />
+        </div>
+        <div style={{ marginBottom: 30 }}>
+          <h3>{t('resetPassword.form.password1')}</h3>
+          <br />
+          <ChangePasswordForm />
+        </div>
+      </Fade>
+    </div>
   );
 }

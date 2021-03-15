70e64d93b6e702853433ab4c2f85b341e11c89e4
Ryan Fleck <ryan.fleck@protonmail.com>
2021-03-14 20:14:23 -0400

    Add SENTRY for error reporting.

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,36 @@ import 'rsuite/dist/styles/rsuite-default.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ReactGA from 'react-ga';
+import * as Sentry from '@sentry/react';
+
+// Returns true in development.
+const dev: boolean =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1' ||
+  window.location.hostname === '';
 
 // THE DEVIL YOU KNOW - SINGULARITY COMETH.
 ReactGA.initialize('UA-192090799-1', {
+  debug: dev,
   gaOptions: {
     cookieDomain: 'auto',
   },
 });
 
+// THE DEVIL YOU DON'T - ERROR REPORTING.
+Sentry.init({
+  debug: dev,
+  environment: dev ? 'development' : 'production',
+  dsn:
+    'https://09d854276f174feaa67947c83611ced8@o464543.ingest.sentry.io/5674705',
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
-      <App />
+      <Sentry.ErrorBoundary fallback={'Crap.'}>
+        <App />
+      </Sentry.ErrorBoundary>
     </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
